c7ec3eba5ac61af8af83b3c75678c7979cece5cc
nds <nds@sanger.ac.uk>
2013-06-27 16:20:02 +0100

    Added a method to extract all the names of repositories i.e organism names

--- modules/ReferenceTrack/Repositories.pm
+++ modules/ReferenceTrack/Repositories.pm
@@ -39,6 +39,19 @@ sub find_by_name
    my ($self,$query) = @_;
    $self->_find_all_by_name_result_set($query)->first;
 }
+
+# Return a list of all the organisms we have data for
+sub find_all_names
+{
+   my ($self) = @_;
+   my @all_results = $self->_find_all_by_name_result_set("")->all;
+   my @organism_names;
+   for my $repo (@all_results){
+   		push( @organism_names, $repo->name);
+   }
+   return \@organism_names;
+}
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
