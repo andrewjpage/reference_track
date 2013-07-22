fdd4dcab8c64cdd38985ca9bb7eeebb5e46f4eb8
Nishadi De Silva <nds@sanger.ac.uk>
2013-07-22 15:47:31 +0100

    Updated comments, and now does git add, commit, fetch and pull

--- reference_track.pl
+++ reference_track.pl
@@ -6,11 +6,11 @@ reference_track.pl
 
 =head1 SYNOPSIS
 
-reference_track.pl [-e (staging|production) ] -q repo_name
+reference_track.pl -q repo_name
 
 =head1 DESCRIPTION
 
-This script allows you to query the reference tracking database.
+This script allows you to create a clone of a repository, perform a git commit or a git pull.
 
 =head1 CONTACT
 
@@ -27,22 +27,32 @@ use Getopt::Long;
 use ReferenceTrack::Repository::Search;
 use ReferenceTrack::Repository::Clone;
 
-my ($database, $query, $clone);
+my ($database, $query, $clone, $add, $message, $major, $update, $help);
 
-GetOptions ('database|d=s'    => \$database,
-            'query|q=s'          => \$query,
-            'clone|c'          => \$clone
+GetOptions ('database|d=s'    => \$database, # Only exposed to developers for testing
+            'query|q=s'       => \$query,
+            'clone|c'         => \$clone,
+            'add|a'			  => \$add,
+            'message|m=s'	      => \$message,
+            'major'			  => \$major,
+            'update|u'		  => \$update,
+            'help|h'		  => \$help,
 );
 
-$query or die <<USAGE;
-Usage: $0 [options]
-Query the reference tracking system
 
-reference_track.pl -c -q repo_name
+(($query && $clone) || ($add && $message) || !$help) or die <<USAGE;
+Usage: $0 [options]
+
+Clone, commit to or update a repository
 
  Options:
-     -q  The name of the repository to look up. It performs a wildcard search '%repo_name%'
-     -c  Take a copy of the repository and put it in the current directory
+     -q  	The name of the repository to look up. It performs a wildcard search '%repo_name%'
+     -c  	Take a copy of the repository and put it in the current directory
+     -a  	Add my changes to the system. You must also enter a short message about the changes using the -m option
+     -m  	Short description about the changes made to the file
+     -major	Can be used with the -a option to indicate a major change
+     -u		Update my files with any changes other people may have made to it
+     -h		Help
 
 USAGE
 ;
@@ -56,18 +66,40 @@ $database_settings{ro_user} = $ENV{VRTRACK_RO_USER}  || 'pathpipe_ro';
 $database_settings{rw_user} =  $ENV{VRTRACK_RW_USER} || 'pathpipe_rw';
 $database_settings{password} = $ENV{VRTRACK_PASSWORD};
 
-
-my $repository_search = ReferenceTrack::Repository::Search->new(
-  database_settings => \%database_settings,
-  query             => $query,
-  );
-$repository_search->print_report();
-
-# Clone the repository and copy over the git hook file
+# Clone the repository (and copy over the git hook file)
 if($clone)
 {
-  ReferenceTrack::Repository::Clone->new(
-    repository_search_results => $repository_search
-  )->clone();
-  
+	my $repository_search = ReferenceTrack::Repository::Search->new(
+  		database_settings => \%database_settings,
+  		query             => $query,
+  	);
+	$repository_search->print_report();
+
+  	ReferenceTrack::Repository::Clone->new(
+    	repository_search_results => $repository_search
+  	)->clone();  
+}
+
+# Run the git add and git commit commands
+if($add)
+{
+	my $git_add_output = `git add .`;
+	if($major){
+		$message = 'MAJOR:'.$message;
+	}
+	my $git_commit_output = `git commit -m "$message"`;
+	# Parse git commit message (check...is this the message that needs to be parsed?)
+	if($git_commit_output =~ m/error/g){
+		print "There appears to be an error with adding your changes. Please contact path-help\@sanger.ac.uk with 
+		       the error message. \n";
+	
+	}
+}
+
+# Run git fetch and git pull 
+if($update)
+{
+	my $git_fetch = `git fetch --all`;
+	my $git_pull = `git pull origin master`;
+	# Parse error messages ?
 }
